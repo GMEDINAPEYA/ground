a62b0548184c325085b72b0ef207f2ad6a40b0f0
GMEDINAPEYA <guido.medina@pedidosya.com>
2023-01-14 13:32:28 -0300

    Add tests for handlers

---
+++ GetGround_Backend_Assignment/backend/golang/cmd/internal/handlers/add_guest_to_list_handler_test.go
@@ -0,0 +1,128 @@
+package handlers
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/getground/tech-tasks/backend/cmd/mocks"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/mock"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestAddGuestToGuestList_Valid_Data(t *testing.T) {
+	// Initialize test data
+	name := "John Doe"
+	table := 1
+	guests := 2
+	jsonStr := fmt.Sprintf(`{"table": %d, "accompanying_guests": %d}`, table, guests)
+	jsonBytes := []byte(jsonStr)
+
+	// Create a new request
+	req, err := http.NewRequest("POST", "/guestlist/"+name, bytes.NewBuffer(jsonBytes))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a new response recorder
+	rr := httptest.NewRecorder()
+
+	service := &mocks.GetGroundService{}
+	service.On("AddGuestToList", mock.Anything, mock.Anything).Return(nil)
+
+	// Create a new handler
+	handler := &BaseHandler{
+		groundService: service,
+	}
+
+	// Create a new mux router and register the handler
+	router := mux.NewRouter()
+	router.HandleFunc("/guestlist/{name}", handler.AddGuestToGuestList).Methods("POST")
+
+	// Execute the request
+	router.ServeHTTP(rr, req)
+
+	// Check the status code
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	// Check the response body
+	expected := fmt.Sprintf(`{"name":"%s"}`, name)
+	actual := strings.TrimSuffix(rr.Body.String(), "\n")
+	if actual != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestAddGuestToGuestList_Invalid_tableId(t *testing.T) {
+	// Initialize test data
+	name := "John Doe"
+	table := 0
+	guests := 2
+	jsonStr := fmt.Sprintf(`{"table": %d, "accompanying_guests": %d}`, table, guests)
+	jsonBytes := []byte(jsonStr)
+
+	// Create a new request
+	req, err := http.NewRequest("POST", "/guestlist/"+name, bytes.NewBuffer(jsonBytes))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a new response recorder
+	rr := httptest.NewRecorder()
+
+	// Create a new handler
+	handler := &BaseHandler{
+		groundService: &mocks.GetGroundService{},
+	}
+
+	// Create a new mux router and register the handler
+	router := mux.NewRouter()
+	router.HandleFunc("/guestlist/{name}", handler.AddGuestToGuestList).Methods("POST")
+
+	// Execute the request
+	router.ServeHTTP(rr, req)
+
+	// Check the status code
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+	}
+}
+
+func TestAddGuestToGuestList_Invalid_accompanying_guests(t *testing.T) {
+	// Initialize test data
+	name := "John Doe"
+	table := 1
+	guests := -3
+	jsonStr := fmt.Sprintf(`{"table": %d, "accompanying_guests": %d}`, table, guests)
+	jsonBytes := []byte(jsonStr)
+
+	// Create a new request
+	req, err := http.NewRequest("POST", "/guestlist/"+name, bytes.NewBuffer(jsonBytes))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a new response recorder
+	rr := httptest.NewRecorder()
+
+	// Create a new handler
+	handler := &BaseHandler{
+		groundService: &mocks.GetGroundService{},
+	}
+
+	// Create a new mux router and register the handler
+	router := mux.NewRouter()
+	router.HandleFunc("/guestlist/{name}", handler.AddGuestToGuestList).Methods("POST")
+
+	// Execute the request
+	router.ServeHTTP(rr, req)
+
+	// Check the status code
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+	}
+}
---
+++ GetGround_Backend_Assignment/backend/golang/cmd/internal/handlers/delete_guest_handler_test.go
@@ -0,0 +1,87 @@
+package handlers
+
+import (
+	"errors"
+	"github.com/getground/tech-tasks/backend/cmd/mocks"
+	"github.com/stretchr/testify/mock"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/gorilla/mux"
+)
+
+func TestDeleteGuest_Success(t *testing.T) {
+	service := &mocks.GetGroundService{}
+	service.On("DeleteGuest", mock.Anything, mock.Anything).Return(nil)
+
+	// Create a new handler
+	handler := &BaseHandler{
+		groundService: service,
+	}
+
+	// create request and response recorder
+	r, _ := http.NewRequest("DELETE", "/guest/John", nil)
+	w := httptest.NewRecorder()
+
+	// add the variable to the request context
+	r = mux.SetURLVars(r, map[string]string{
+		"name": "John",
+	})
+
+	// call the handler
+	handler.DeleteGuest(w, r)
+
+	// assert the response
+	assert.Equal(t, http.StatusNoContent, w.Code)
+	assert.Empty(t, w.Body.String())
+}
+
+func TestDeleteGuest_Missing_Name(t *testing.T) {
+	service := &mocks.GetGroundService{}
+
+	// Create a new handler
+	handler := &BaseHandler{
+		groundService: service,
+	}
+
+	// create request and response recorder
+	r, _ := http.NewRequest("DELETE", "/guest/", nil)
+	w := httptest.NewRecorder()
+
+	// call the handler
+	handler.DeleteGuest(w, r)
+
+	// assert the response
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, "missing user name. Name can not be empty", strings.TrimSuffix(w.Body.String(), "\n"))
+}
+
+func TestDeleteGuest_Failure(t *testing.T) {
+	service := &mocks.GetGroundService{}
+	service.On("DeleteGuest", mock.Anything, mock.Anything).Return(errors.New("unable to delete guest"))
+
+	// Create a new handler
+	handler := &BaseHandler{
+		groundService: service,
+	}
+
+	// create request and response recorder
+	r, _ := http.NewRequest("DELETE", "/guest/John", nil)
+	w := httptest.NewRecorder()
+
+	// add the variable to the request context
+	r = mux.SetURLVars(r, map[string]string{
+		"name": "John",
+	})
+
+	// call the handler
+	handler.DeleteGuest(w, r)
+
+	// assert the response
+	assert.Equal(t, http.StatusInternalServerError, w.Code)
+	assert.Equal(t, "unable to delete guest", strings.TrimSuffix(w.Body.String(), "\n"))
+}
